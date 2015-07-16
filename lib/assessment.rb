8c0d80dcf3de6c6422dedd812da38e63d132441e
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-16 13:58:11 -0400

    completes assessments with code tested using pry. however, testing with the spec/assessment_spec file still causes two failures, though it seems to be working OK when tested with pry

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -6,13 +6,20 @@ Answer = OpenStruct.new
 # Make all of those attributes read-only.
 
 ##
-# your code here
+class Dog
+  def initialize(name, breed, age)
+    @name, @breed, @age = name, breed, age
+  end
+
+  attr_reader :name, :breed, :age
+
+end
 ##
 
 # Question 2
 # Create a new Dog object.
 
-Answer.dog = nil # replace nil with your code
+Answer.dog = Dog.new("luca", "mutt", 2)
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
@@ -20,20 +27,32 @@ Answer.dog = nil # replace nil with your code
 # Name and location should be writeable, but age should be read-only.
 
 ##
-# your code here
+class Person
+  def initialize(name, age, location)
+    @name, @age, @location = name, age, location
+  end
+
+  attr_reader :age
+  attr_accessor :name, :location
+
+end
 ##
 
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
-Answer.dave = nil # replace nil with your code
-Answer.dave.location = nil # replace nil with your code
+Answer.dave = Person.new("Dave",32,"Ohio")
+Answer.dave.location = "Somerville"
 
 # Question 5
 # Create another class called Developer that inherits from Person.
 # Give it a new public method called 'hire_for_job'
 
 ##
-# your code here
+class Developer < Person
+  def hire_for_job
+    puts "you're hired, #{name}!"
+  end
+end
 ##
