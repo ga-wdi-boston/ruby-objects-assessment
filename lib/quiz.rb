e863e668f7cc72bc819bc6ebc4cf213051ac1760
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-16 12:12:42 -0400

    Update quiz

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,20 +1,39 @@
+require 'ostruct'
+Answer = OpenStruct.new
+
 # Question 1
 # Create a class called Dog. It should have, as attributes, name, breed, and age.
 # Make all of those attributes read-only.
 
+##
+# your code here
+##
+
 # Question 2
 # Create a new Dog object.
+
+Answer.dog = nil # replace nil with your code
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
 # It should set all three attributes when you create a new Person.
 # Name and location should be writeable, but age should be read-only.
 
+##
+# your code here
+##
+
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
+Answer.dave = nil # replace nil with your code
+Answer.dave.location = nil # replace nil with your code
+
 # Question 5
 # Create another class called Developer that inherits from Person.
-# Give it a new private method called 'write_code', which prints out "TYPE TYPE TYPE".
-# Also give it a new public method called 'hire_for_job', which calls 'write_code'
+# Give it a new public method called 'hire_for_job'
+
+##
+# your code here
+##
