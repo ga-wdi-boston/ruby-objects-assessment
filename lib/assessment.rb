84391b758e8db3bc6f7e0b4c82816767f96428f1
Teo Paolucci <teo.paolucci@gmail.com>
2015-10-23 09:23:38 -0400

    Finished assessment.rb
    Comfort & Clarity: I r8 gr8 m8, 8 outta 8

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -6,13 +6,19 @@ Answer = OpenStruct.new
 # Make all of those attributes read-only.
 
 ##
-# your code here
+class Dog
+  attr_reader :name, :breed, :age
+
+  def initialize (name, breed, age)
+    @name, @breed, @age = name, breed, age
+  end
+end
 ##
 
 # Question 2
 # Create a new Dog object.
 
-Answer.dog = nil # replace nil with your code
+Answer.dog = Dog.new("Fido", "Dalmatian", 7)
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
@@ -20,20 +26,31 @@ Answer.dog = nil # replace nil with your code
 # Name and location should be write-only, but age should be read-only.
 
 ##
-# your code here
+class Person
+  attr_reader :age
+  attr_writer :name, :location
+
+  def initialize (name, age, location)
+    @name, @age, @location = name, age, location
+  end
+end
 ##
 
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
-Answer.dave = nil # replace nil with your code
-Answer.dave.location = nil # replace nil with your code
+Answer.dave = Person.new("Dave", 32, "Ohio")
+Answer.dave.location = "Somerville"
 
 # Question 5
 # Create another class called Developer that inherits from Person.
 # Give it a new public method called 'hire_for_job'
 
 ##
-# your code here
+class Developer < Person
+  def hire_for_job
+    puts "Thanks for hiting me!"
+  end
+end
 ##
