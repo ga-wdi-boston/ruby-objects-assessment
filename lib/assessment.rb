77dfd2c75442890e967efaa15560c2c989e05615
EdieShimel <edieshimel@gmail.com>
2015-10-23 09:51:05 -0400

    3 clarity....not sure I understood all the git hullabaloo, but I think I understood the assessment questions 4
    My notes, the readme from yesterday.

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,14 +5,25 @@ Answer = OpenStruct.new
 # Create a class called Dog. It should have, as attributes, name, breed, and age.
 # Make all of those attributes read-only.
 
-##
-# your code here
+class Dog
+  attr_reader :name
+  attr_reader :breed
+  attr_reader :age
+
+# This is in case I am supposed to set the attributes
+def initialize(name, breed, age)
+  @name = name
+  @breed = breed
+  @age = age
+end
+end
 ##
 
 # Question 2
 # Create a new Dog object.
 
-Answer.dog = nil # replace nil with your code
+Answer.dog = pooch = Dog.new("Pooch", "poodle", 3)
+
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
@@ -20,20 +31,43 @@ Answer.dog = nil # replace nil with your code
 # Name and location should be write-only, but age should be read-only.
 
 ##
-# your code here
+class Person
+  attr_writer :name
+  attr_reader :age
+  attr_writer :location
+
+
+  def initialize(name, age, location)
+
+    @name = name
+    @age = age
+    @location = location
+  end
+end
 ##
 
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
-Answer.dave = nil # replace nil with your code
-Answer.dave.location = nil # replace nil with your code
+Answer.dave = dave = Person.new("Dave", 32, "Ohio") # replace nil with your code
+Answer.dave.location = dave.location.new = dave.location.gsub(/Ohio/, 'Somerville') # replace nil with your code
 
 # Question 5
 # Create another class called Developer that inherits from Person.
 # Give it a new public method called 'hire_for_job'
 
 ##
-# your code here
+# Should I assume that class Person already exists?  If not
+
+class Person
+  attr_reader :something
+def something
+end
+
+class Developer < Person
+  def hire_for_job
+  end
 ##
+
+
