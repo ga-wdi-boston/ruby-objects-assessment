87c66c58352472079ba6e1fb0b3683eb1c776963
Grace <gkamwati@yahoo.com>
2015-10-23 09:42:52 -0400

    2nd commit

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -23,7 +23,7 @@ class Dog
 # Question 2
 # Create a new Dog object.
 
-Answer.dog = nil # replace nil with your code
+Answer.dog = kipper.new  # replace nil with your code
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
@@ -32,13 +32,21 @@ Answer.dog = nil # replace nil with your code
 
 ##
 # your code here
+class Person
+  attr_accessor :name :location
+  def initialize(name,age,location)
+      @name = name
+      @age = age
+      @location = location
+    end
+  end
 ##
 
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
-Answer.dave = nil # replace nil with your code
+Answer.dave = dave.new 32 "Ohio" # replace nil with your code
 Answer.dave.location = nil # replace nil with your code
 
 # Question 5
@@ -47,4 +55,16 @@ Answer.dave.location = nil # replace nil with your code
 
 ##
 # your code here
+class Developer < Person
+
+class Developer
+  attr_accessor :hire_for_job
+  def initialize
+    super
+    @controller = eval "#{self.class.name}Controller.new(self)"
+  end
+end
+
+class Developer <Person
+end
 ##
