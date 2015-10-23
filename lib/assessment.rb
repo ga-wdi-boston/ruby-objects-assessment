7a7404c01a9234c0065a6624e9f6c4a19f307242
resource11 <mcmahon@resource11.com>
2015-10-23 09:37:52 -0400

    added responses to assessment.rb file on response branch

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -6,13 +6,19 @@ Answer = OpenStruct.new
 # Make all of those attributes read-only.
 
 ##
-# your code here
+class Dog
+  attr_reader :name, :breed, :age
+  def initialize(name, breed, age)
+    @name, @breed, @age = name, breed, age
+  end
+end
 ##
 
 # Question 2
 # Create a new Dog object.
 
-Answer.dog = nil # replace nil with your code
+# In my attempt to learn how to read/interpret a question correctly, I didn't pass in parameters the first time and received an error. Passed in parameters and the test passed. So... if you create a class and set parameters on the initialize instance method, and then invoke an new instance of that class, you must pass in arguments for the initialize instance method to run without error, yes?
+Answer.dog = Dog.new("rover", "pomeranian", 26)
 
 # Question 3
 # Create a Person class, with attributes name, age, and location.
@@ -20,20 +26,30 @@ Answer.dog = nil # replace nil with your code
 # Name and location should be write-only, but age should be read-only.
 
 ##
-# your code here
+class Person
+  attr_writer :name, :location
+  attr_reader :age
+  def initialize(name, age, location)
+    @name, @age, @location = name, age, location
+  end
+end
 ##
 
 # Question 4
 # Create a new Person object using parameters "Dave", 32, and "Ohio".
 # Then, set that Person's location to "Somerville"
 
-Answer.dave = nil # replace nil with your code
-Answer.dave.location = nil # replace nil with your code
+Answer.dave = Person.new("Dave", 32, "Ohio")
+Answer.dave.location = "Somerville"
+
 
 # Question 5
 # Create another class called Developer that inherits from Person.
 # Give it a new public method called 'hire_for_job'
 
 ##
-# your code here
+class Developer < Person
+  def hire_for_job
+  end
+end
 ##
