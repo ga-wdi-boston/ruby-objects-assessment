064ffd425ce5b29b889662b492d809cc07a037c9
EchoXin <xin.yi@husky.neu.edu>
2016-08-08 09:28:20 -0400

    Add response

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -24,7 +24,16 @@ Response = OpenStruct.new
 # be writable.
 
 ##
-# your response here
+class Person
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+  attr_accessor :name
+  attr_reader :age
+  attr_writer :location
+end
 ##
 
 ##
@@ -33,7 +42,8 @@ Response = OpenStruct.new
 # "Somerville". Finally, assign the modified Person to `Response.dave` below.
 
 ## replace nil with your response, then continue your work on the next line
-dave = nil
+dave = Person.new("Dave", 32, "Ohio")
+dave.location = "Somerville"
 Response.dave = dave
 ##
 
@@ -43,7 +53,11 @@ Response.dave = dave
 # which returns the string "think think think".
 
 ##
-# your response here
+class Developer < Person
+  def solve_problems
+    "think think think"
+  end
+end
 ##
 
 ##
@@ -74,10 +88,10 @@ end
 
 ## What will be the output from calling `HouseCat.new.say_hello`?
 # replace nil with your response
-Response.housecat_noise = nil
+Response.housecat_noise = "I am a HouseCat, and I go '#{@sound}'"
 
 ## Explain why this would be the output, based on the method lookup chain.
-# your response as a comment here
+{self.class.name} point to the class name,which is HouseCat, while'#{@sound}'is a string.
 ##
 
 ##
@@ -97,10 +111,14 @@ module Carnivorous
       puts 'Yuck!'
     end
   end
-end
 
 ##
-# your response here
+class Lion < Cat
+  include Carnivorous
+  def roar
+    p "ROAR"
+  end
+end
 ##
 
 # #
@@ -108,7 +126,7 @@ end
 # over using direct inheritance?
 
 ##
-# your response as a comment here
+avoid dry
 ##
 
 ##
@@ -126,13 +144,13 @@ class ComboAttack
     @damage = 0
   end
 
-  def punch
+  def self.punch
     @moves << 'punch'
     @damage += 5
     @damage *= multiplier
   end
 
-  def move(direction)
+  def self.move(direction)
     @moves << "move #{direction}"
   end
 
@@ -140,6 +158,10 @@ class ComboAttack
     @moves << 'kick'
     @damage += 10
     @damage *= multiplier
+  end
+
+  def get_possible_moves
+    "kick, move, punch"
   end
 
   private
