6e2bebd6b66419745d098941cac438aa635d6b5e
jenboyd <jboyd6290@gmail.com>
2016-09-20 09:28:37 -0400

    Ruby Objects Diagnostic Response

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -24,7 +24,17 @@ Response = OpenStruct.new
 # be writable.
 
 ##
-# your response here
+class Person
+  attr_accessor :name
+  attr_reader :age
+  attr_writer :location
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+end
+
 ##
 
 ##
@@ -33,7 +43,8 @@ Response = OpenStruct.new
 # "Somerville". Finally, assign the modified Person to `Response.dave` below.
 
 ## replace nil with your response, then continue your work on the next line
-dave = nil
+dave = Person.new("Dave", 32, "Ohio")
+dave.location = "Somerville"
 Response.dave = dave
 ##
 
@@ -43,8 +54,14 @@ Response.dave = dave
 # which returns the string "think think think".
 
 ##
-# your response here
+class Developer < Person
+
+  def solve_problems
+    "think think think"
+  end
+end
 ##
+
 
 ##
 # Study the code below before responding.
@@ -74,12 +91,12 @@ end
 
 ## What will be the output from calling `HouseCat.new.say_hello`?
 # replace nil with your response
-Response.housecat_noise = nil
+Response.housecat_noise = I am a HouseCat, and I go 'meow'
 
 ## Explain why this would be the output, based on the method lookup chain.
 # your response as a comment here
 ##
-
+#HouseCat inherits from Cat, which inherits from Animal where say_hello is defined.
 ##
 # Define a new class, 'Lion', which (a) inherits from 'Cat',
 # (b) uses the 'Carnivorous' module below as a mixin, and
@@ -100,7 +117,14 @@ module Carnivorous
 end
 
 ##
-# your response here
+class Lion < Cat
+  include Carnivorous
+
+  def Roar
+    "ROAR"
+  end
+end
+
 ##
 
 # #
@@ -108,7 +132,8 @@ end
 # over using direct inheritance?
 
 ##
-# your response as a comment here
+# You can attach as many mixins as you want to a class. With direct inheritance, you can only
+# reference a single class.
 ##
 
 ##
