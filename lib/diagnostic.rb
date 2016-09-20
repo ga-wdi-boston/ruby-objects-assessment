1f50e87d8a39356334e4329cdcdaba467edf4622
tuckyeah <h.rosebrock@gmail.com>
2016-09-20 09:13:23 -0400

    complete diagnostic -- answer all questions

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -24,7 +24,17 @@ Response = OpenStruct.new
 # be writable.
 
 ##
-# your response here
+class Person
+  attr_reader :age
+  attr_writer :location
+  attr_accessor :name
+
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+end
 ##
 
 ##
@@ -33,7 +43,8 @@ Response = OpenStruct.new
 # "Somerville". Finally, assign the modified Person to `Response.dave` below.
 
 ## replace nil with your response, then continue your work on the next line
-dave = nil
+dave = Person.new('Dave', 32, 'Ohio')
+dave.location = 'Somerville'
 Response.dave = dave
 ##
 
@@ -43,7 +54,11 @@ Response.dave = dave
 # which returns the string "think think think".
 
 ##
-# your response here
+class Developer < Person
+  def solve_problems
+    'think think think'
+  end
+end
 ##
 
 ##
@@ -74,11 +89,14 @@ end
 
 ## What will be the output from calling `HouseCat.new.say_hello`?
 # replace nil with your response
-Response.housecat_noise = nil
+Response.housecat_noise = "I am a HouseCat, and I go 'meow'"
 
 ## Explain why this would be the output, based on the method lookup chain.
 # your response as a comment here
 ##
+ # @sound is initalized in the Animal class, which HouseCat inherits from, but is
+ # overwritten in the instantiation of an instance of HouseCat. say_hello is an instance
+ # method inherited from Animal by HouseCat
 
 ##
 # Define a new class, 'Lion', which (a) inherits from 'Cat',
@@ -100,7 +118,12 @@ module Carnivorous
 end
 
 ##
-# your response here
+class Lion < Cat
+  include Carnivorous
+  def roar
+    puts 'ROAR!'
+  end
+end
 ##
 
 # #
@@ -108,7 +131,8 @@ end
 # over using direct inheritance?
 
 ##
-# your response as a comment here
+# Ruby is single inheritance, so if we want to include behaviors from multiple
+# different places, it is best to use mixins
 ##
 
 ##
@@ -130,16 +154,23 @@ class ComboAttack
     @moves << 'punch'
     @damage += 5
     @damage *= multiplier
+    self
   end
 
   def move(direction)
     @moves << "move #{direction}"
+    self
   end
 
   def kick
     @moves << 'kick'
     @damage += 10
     @damage *= multiplier
+    self
+  end
+
+  def self.get_possible_moves
+    'kick, move, punch'
   end
 
   private
