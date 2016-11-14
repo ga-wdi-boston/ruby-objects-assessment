e5d8f9078a1a3bebf46400c123f8953970ba10f8
ielole <ieloletin@gmail.com>
2016-11-14 09:25:35 -0500

    Response to ruby-object-diagnostic.
    Attempted to answer questions.

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -24,7 +24,17 @@ Response = OpenStruct.new
 # be writable.
 
 ##
-# your response here
+class Person
+  attr_accessor :name
+  attr_reader :age
+  attr_writer :location
+
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+end
 ##
 
 ##
@@ -33,7 +43,7 @@ Response = OpenStruct.new
 # "Somerville". Finally, assign the modified Person to `Response.dave` below.
 
 ## replace nil with your response, then continue your work on the next line
-dave = nil
+dave = Person.new('Dave', 32, 'Ohio')
 Response.dave = dave
 ##
 
@@ -43,7 +53,11 @@ Response.dave = dave
 # which returns the string "think think think".
 
 ##
-# your response here
+class Developer < Person
+  def solve_problems
+    'think think think'
+  end
+end
 ##
 
 ##
@@ -74,10 +88,12 @@ end
 
 ## What will be the output from calling `HouseCat.new.say_hello`?
 # replace nil with your response
-Response.housecat_noise = nil
+Response.housecat_noise = "I am a HouseCat, and I go 'meow'"
 
 ## Explain why this would be the output, based on the method lookup chain.
-# your response as a comment here
+The class HouseCat inherits the say_hello method from the Cat class, which
+inherited it from the Animal class, but the HouseCat has "it's" own initialize
+method so it will say 'meow'.
 ##
 
 ##
@@ -100,7 +116,9 @@ module Carnivorous
 end
 
 ##
-# your response here
+class Lion < Cat
+
+end
 ##
 
 # #
