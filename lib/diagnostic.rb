ff6693924f173239b1c3967df0e78781f4d8c807
lbreguet <louis-w@live.com>
2017-02-06 09:26:27 -0500

    Diagnostic complete
    -comfort: 3
    -clarity: 4
    -use of class notes and ruby doc

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -26,7 +26,16 @@ Response = OpenStruct.new
 # be writable.
 
 ##
-# your response here
+class Person
+  attr_reader :age
+  attr_writer :location
+  attr_accessor :name
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+end
 ##
 
 ##
@@ -35,7 +44,7 @@ Response = OpenStruct.new
 # "Somerville". Finally, assign the modified Person to `Response.dave` below.
 
 ## replace nil with your response, then continue your work on the next line
-dave = nil
+dave = Person.new("Dave", 32, "Ohio")
 Response.dave = dave
 ##
 
@@ -45,7 +54,11 @@ Response.dave = dave
 # which returns the string "think think think".
 
 ##
-# your response here
+class Developer < Person
+  def solve_problems
+    p "think think think"
+  end
+end
 ##
 
 ##
@@ -78,10 +91,11 @@ end
 
 ## What will be the output from calling `HouseCat.new.say_hello`?
 # replace nil with your response
-Response.housecat_noise = nil
+Response.housecat_noise = "I am a HouseCat and I go 'meow'"
 
 ## Explain why this would be the output, based on the method lookup chain.
-# your response as a comment here
+# HouseCat inherits from Cat, which in turn inherits from Animal, where the
+# method say_hello exists.
 ##
 
 ##
@@ -105,7 +119,12 @@ module Carnivorous
 end
 
 ##
-# your response here
+class Lion < Cat
+  include Carnivorous
+  def roar
+    p 'ROAR'
+  end
+end
 ##
 
 # #
@@ -113,7 +132,7 @@ end
 # over using direct inheritance?
 
 ##
-# your response as a comment here
+"Don't know."
 ##
 
 ##
@@ -136,16 +155,23 @@ class ComboAttack
     @moves << 'punch'
     @damage += 5
     @damage *= multiplier
+    self
   end
 
   def move(direction)
     @moves << "move #{direction}"
+    self
   end
 
   def kick
     @moves << 'kick'
     @damage += 10
     @damage *= multiplier
+    self
+  end
+
+  def get_possible_moves
+    p 'kick, move, punch'
   end
 
   private
